0e27240220369f01584a8a642e23123ec42e40bd
AuraRizz <205314701+AuraRizz@users.noreply.github.com>
2025-05-10 13:11:53 +0200

    Initial

---
+++ explosions/explosió.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+func _ready():
+	$Casa/Normal.visible = true
+	$Casa/Incendiada.visible = false
+	$Foc/Foc.emitting = false
+	$Foc/Foc2.emitting = false
+	$Foc/Foc3.emitting = false
+	$Foc/Foc4.emitting = false
+	$Foc/Foc5.emitting = false
+	$Foc/Foc6.emitting = false
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		$Explosio/CPUParticles2D.emitting = true
+		$Casa/Normal.visible = false
+		$Casa/Incendiada.visible = true
+		$Foc/Foc.emitting = true
+		$Foc/Foc2.emitting = true
+		$Foc/Foc3.emitting = true
+		$Foc/Foc4.emitting = true
+		$Foc/Foc5.emitting = true
+		$Foc/Foc6.emitting = true
